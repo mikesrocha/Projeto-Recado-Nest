8b03c22c055d96a475635439acd8c625140486ae
mikesrocha <mikesrocha8@gmail.com>
2026-07-31 15:03:22 -0400

    Atualiza arquivos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //revome chaves que não estão no dto.
+      forbidNonWhitelisted: true, //levanta erro quando a chave não existir
+      transform: false, //tenta transofrmar os tipos de dados de param e dtos
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
   //test
 }
--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -6,6 +6,7 @@ import {
   //HttpCode,
   //HttpStatus,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
@@ -27,7 +28,7 @@ export class RecadosController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.recadosService.findOne(id);
   }
 
@@ -42,7 +43,8 @@ export class RecadosController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
+    console.log(id, typeof id);
     return this.recadosService.remove(id);
   }
 }
